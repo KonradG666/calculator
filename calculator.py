c5c81b2622f30bf507c230e8ebc9639abaa7b893
Konrad Gawron <konrad.gawron@poczta.fm>
2020-11-15 12:48:44 +0100

    Set condition and and input

--- calculator.py
+++ calculator.py
@@ -6,4 +6,26 @@ def calculate():
     3 Mnożenie, 
     4 Dzielenie, 
     5 Exit: 
-    """)) 
+    """))
+
+    if operation in (1, 2, 3, 4, 5):
+
+        num1 = float(input("Podaj pierwszą liczbę: "))
+        num2 = float(input("Podaj drugą liczbę: "))
+
+        if operation == 1:
+            print("Dodaję %s i %s" % (num1, num2))
+            print("Wynik to: ", (num1 + num2))
+        elif operation == 2:
+            print("Odejmuję %s i %s" % (num1, num2))
+            print("Wynik to: ", (num1 - num2))
+        elif operation == 3:
+            print("Mnożę %s i %s" % (num1, num2))
+            print("Wynik to: ", (num1 * num2))
+        elif operation == 4:
+            print("Dzielę %s i %s" % (num1, num2))
+            print("Wynik to: ", (num1 / num2))
+        elif operation == 5:
+            end()
+
+    repeat()
